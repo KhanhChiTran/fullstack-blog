c80f5bdb5178024359a5f4a9b02acb8cea963441
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-12 22:43:41 +0200

    Lets ROCK

--- api/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const server = express()
-
-server.get("/", (req, res) => {
-  res.send("Hello World, Done Basic setup")
-})
-
-server.listen(5000, () => console.log("app is running"))
--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express")
 
 const server = express()
 
-server.get("/", (req, res) => {
-  res.send("Hello World, Done Basic setup")
-})
+server.use(express.static("ui/build"))
+
+// server.get("/", (req, res) => {
+//   res.send("Hello World, Done Basic setup")
+// })
 
 server.listen(process.env.PORT || 5000, () => console.log("app is running"))
