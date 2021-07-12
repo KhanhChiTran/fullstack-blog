2be12d7c355dc327fefbc5f7ec93ca1b5284b5ed
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-12 22:43:20 +0200

    Lets ROCK

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg"
 import "./App.css"
+import { Route, Switch, Link } from "react-router-dom"
+import About from "./pages/about"
+import Login from "./pages/login"
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my Blog !!</h1>
       <span>Done Basic setup</span>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   )
 }
