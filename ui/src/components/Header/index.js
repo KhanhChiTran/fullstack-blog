94045e456b8f1eed963ce9badf1e1c245c192df4
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-13 15:17:26 +0200

    navbar done

--- ui/src/components/Header/index.js
+++ ui/src/components/Header/index.js
@@ -1,10 +1,53 @@
 import React from "react"
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Navbar bg="light" expand="md">
+      <Container>
+        <Navbar.Brand>
+          {" "}
+          <Link to="/home" className="nav-link">
+            {" "}
+            Home
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Link to="/home" className="nav-link">
+                {" "}
+                Home
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/contact" className="nav-link">
+                {" "}
+                Contact
+              </Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Link to="/about" className="nav-link">
+                {" "}
+                About
+              </Link>
+            </Nav.Item>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.ItemText>
+                <Link to="/login"> Login</Link>
+              </NavDropdown.ItemText>
+              <NavDropdown.ItemText>
+                <Link to="/registration"> Sign Up</Link>
+              </NavDropdown.ItemText>
+              <NavDropdown.ItemText></NavDropdown.ItemText>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
--- ui/src/components/Layout/index.js
+++ ui/src/components/Layout/index.js
@@ -1,8 +1,21 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 
+import Header from "../Header"
+import Footer from "../Footer"
+import "./layout.css"
+
 const Layout = ({ children }) => {
-  return <Container>{children}</Container>
+  return (
+    <Container fluid className="layout">
+      <Header />
+      <Container className="layout-header"></Container>
+      <Container className="layout-main">{children}</Container>
+      <Container className="layout-footer">
+        <Footer />
+      </Container>
+    </Container>
+  )
 }
 
 export default Layout
