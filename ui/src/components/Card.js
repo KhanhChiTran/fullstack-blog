c62abf2014cee5f181136d912fe2020c9b40061e
Chi Tran <khanhchi.tran13051@gmail.com>
2021-07-18 20:06:37 +0200

    backdrop-filter blur card when hovered on it

--- ui/src/components/Card.js
+++ ui/src/components/Card.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Card, Button } from "react-bootstrap"
+import "./card.css"
 
 export default function BlogCard({ Img, Title }) {
   return (
-    <Card style={{ width: "100%" }}>
+    <Card className="wrap-card" style={{ width: "100%" }}>
+      <div className="overlay"></div>
       <Card.Img style={{ height: 250 }} variant="top" src={Img} />
       <Card.Body>
         <Card.Title>{Title}</Card.Title>
@@ -12,9 +14,7 @@ export default function BlogCard({ Img, Title }) {
           deserunt omnis quaerat possimus eaque molestias dolorum voluptatem
           nam...
         </Card.Text>
-        <Button style={{ width: "100%" }} variant="primary">
-          Read more
-        </Button>
+        <Button variant="primary">Read more</Button>
       </Card.Body>
     </Card>
   )
